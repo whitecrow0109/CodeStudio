c6a4f31c868f208305c799925584bcb449bfc87d
whitecrow0109 <whitecrow0109@gmail.com>
2023-04-13 17:11:49 +0900

    Sort 0 1 2

---
+++ Medium/Sort_0_1_2.py
@@ -0,0 +1,48 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+from sys import stdin,setrecursionlimit
+setrecursionlimit(10**7)
+
+def sort012(arr, n) : 
+    cnt = [0, 0, 0]
+    for i in range(n):
+        cnt[arr[i]] += 1
+    ans = []
+    for i in range(3):
+        for j in range(cnt[i]):
+            ans.append(i)
+    for i in range(n): arr[i] = ans[i]
+
+
+#taking inpit using fast I/O
+def takeInput() :
+	n = int(input().strip())
+
+	if n == 0 :
+		return list(), 0
+
+	arr = list(map(int, stdin.readline().strip().split(" ")))
+
+	return arr, n
+
+
+
+def printAnswer(arr, n) :
+    
+    for i in range(n) :
+        
+        print(arr[i],end=" ")
+        
+    print()
+    
+#main
+
+t = int(input().strip())
+for i in range(t) :
+
+    arr, n= takeInput()
+    sort012(arr, n)
+    printAnswer(arr, n)
